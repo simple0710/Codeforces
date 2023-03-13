3ae9c7175db97247b60885092dd68b96a3456155
simple710 <thesimpleman@naver.com>
2023-03-13 16:55:05 +0900

    Nebius Welcome Round (Div. 1 + Div. 2)

--- Nebius Welcome Round (Div. 1 + Div. 2)/B.Vaccination.py
+++ Nebius Welcome Round (Div. 1 + Div. 2)/B.Vaccination.py
@@ -4,14 +4,14 @@ import sys
 input = sys.stdin.readline
 
 for _ in range(int(input())):
-  # 환자, 팩 용량, 백신 수명, 최대 기다리는 순간
+  # 환자, 팩 용량, 백신 수명, 최대 기다리는 시간
   n, k, d, w = map(int,input().split())
   arr = list(map(int,input().split()))
   s = k # 현재 용량
   op = arr[0] # 개봉한 시점
   res = 1
   for p in arr:
-    # 팩이 남았고, 팩을 개봉한 시점 + 수명 >= 환자 대기 시간
+    # 백신이 남았고, 팩을 개봉한 시점 + 수명 >= 환자 대기 시간
     if s and op + d >= p - w:
       s -= 1
       continue
